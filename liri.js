3af4f691bf149739420f1b79a6d5cc53786c1f07
Rubentba <rubenatorresbeltran@gmail.com>
2019-05-19 14:34:34 -0400

    Commented as much as possible and fixed some indentation problems.

--- liri.js
+++ liri.js
@@ -28,6 +28,7 @@ spotifyThis = function(song) {
         } else { 
             // For Loop needed to catch the first artist found when API is called.
             for (var i = 0; i < data.tracks.items[0].artists.length; i++) {
+                // Appends response and stores it within log.txt.
                 fs.appendFile("log.txt",`\nArtist:        ${data.tracks.items[0].artists[0].name}\n` + `Song:         ${data.tracks.items[0].name}\n` + `Preview Link: ${data.tracks.items[0].preview_url}\n` + `Album:        ${data.tracks.items[0].album.name}\n`, function(err) {
                     if (err) {
                         console.log(err)
@@ -43,53 +44,65 @@ spotifyThis = function(song) {
     })
 }
 
+// Calls OMDB API and displays the listed parameters in the console.
 movieThis = function(movie) {
-        if(!movie) {
-            movie = "Mr. Nobody"
-        }
-
-        let URL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
-
-        axios.get(URL).then(function(response) {
-            fs.appendFile("log.txt",`\nMovie title:            ${response.data.Title}\n` + `Release year:           ${response.data.Year}\n` + `IMBD rating:            ${response.data.imdbRating}\n` + `Rotten Tomatoes rating: ${response.data.Ratings[1].Value}\n` + `Country where produced: ${response.data.Country}\n` + `Languages:              ${response.data.Language}\n` + `Actors:                 ${response.data.Actors}\n`, function(err) {
-                if (err) {
-                    console.log(err)
-                }
-            })
-            console.log(`Movie title:            ${response.data.Title}`)
-            console.log(`Release year:           ${response.data.Year}`)
-            console.log(`IMBD rating:            ${response.data.imdbRating}`)
-            console.log(`Rotten Tomatoes rating: ${response.data.Ratings[1].Value}`)
-            console.log(`Country where produced: ${response.data.Country}`)
-            console.log(`Languages:              ${response.data.Language}`)
-            console.log(`Actors:                 ${response.data.Actors}`)
+    // If no parameter is entered, default to this song.
+    if(!movie) {
+        movie = "Mr. Nobody"
+    }
+    // Variable that stores API url to then be used within axios.
+    let URL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
+    // Axios calls OMDB API and returns information to console.
+    axios.get(URL).then(function(response) {
+        // Appends response and stores it within log.txt.
+        fs.appendFile("log.txt",`\nMovie title:            ${response.data.Title}\n` + `Release year:           ${response.data.Year}\n` + `IMBD rating:            ${response.data.imdbRating}\n` + `Rotten Tomatoes rating: ${response.data.Ratings[1].Value}\n` + `Country where produced: ${response.data.Country}\n` + `Languages:              ${response.data.Language}\n` + `Actors:                 ${response.data.Actors}\n`, function(err) {
+            // Catches error then displays it.
+            if (err) {
+                console.log(err)
+            }
         })
+        // Displays caught information.
+        console.log(`Movie title:            ${response.data.Title}`)
+        console.log(`Release year:           ${response.data.Year}`)
+        console.log(`IMBD rating:            ${response.data.imdbRating}`)
+        console.log(`Rotten Tomatoes rating: ${response.data.Ratings[1].Value}`)
+        console.log(`Country where produced: ${response.data.Country}`)
+        console.log(`Languages:              ${response.data.Language}`)
+        console.log(`Actors:                 ${response.data.Actors}`)
+    })
 }
 
+// Calls BandsInTown API and displays the listed parameters in the console.
 concertThis = function(artist) {
-
-        let URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-
-        axios.get(URL).then(function(response) {
-            fs.appendFile("log.txt",`\nName of the venue: ${response.data[0].venue.name}\n` + `Venue location:    ${response.data[0].venue.city + ", " + response.data[0].venue.country}\n` + `Event Date:        ${moment(response.data[0].datetime).format("MM-DD-YYYY")}\n`, function(err) {
-                if (err) {
-                    console.log(err)
-                }
-            })
-            console.log(`Name of the venue: ${response.data[0].venue.name}`)
-            console.log(`Venue location:    ${response.data[0].venue.city + ", " + response.data[0].venue.country}`)
-            console.log(`Event Date:        ${moment(response.data[0].datetime).format("MM-DD-YYYY")}`)
+    // Variable that stores API url to then be used within axios.
+    let URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    // Axios calls OMDB API and returns information to console.
+    axios.get(URL).then(function(response) {
+        // Appends response and stores it within log.txt.
+        fs.appendFile("log.txt",`\nName of the venue: ${response.data[0].venue.name}\n` + `Venue location:    ${response.data[0].venue.city + ", " + response.data[0].venue.country}\n` + `Event Date:        ${moment(response.data[0].datetime).format("MM-DD-YYYY")}\n`, function(err) {
+            // Catches error then displays it.
+            if (err) {
+                console.log(err)
+            }
         })
+        // Displays caught information.
+        console.log(`Name of the venue: ${response.data[0].venue.name}`)
+        console.log(`Venue location:    ${response.data[0].venue.city + ", " + response.data[0].venue.country}`)
+        console.log(`Event Date:        ${moment(response.data[0].datetime).format("MM-DD-YYYY")}`)
+    })
 }
 
+// Calls what is in random.txt and displays the listed parameters in the console.
 doWhatThis = function() {
     fs.readFile("random.txt", "utf8", function(error, data) {
+        // Catches error then displays it.
         if (error) {
             console.log(error)
         }else {
-
+            // Variable that stores data from random.txt and seperates it by the commas within it.
             let randomText = data.split(",")
                 term = randomText[1]
+            // Calls spotifyThis within random text.
             spotifyThis(randomText[1])
         }
     })
